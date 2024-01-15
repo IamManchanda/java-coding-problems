9a6c807c11dc37dd224191d7dcf70ba0a8b6c593
Harry Manchanda <harmanmanchanda182@gmail.com>
2024-01-15 18:00:56 +0530

    progress with problem 7

---
+++ 007-print-largest-of-two-numbers/001-solution/Main.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        /**
+         * Problem: Print the largest of two numbers.
+         */
+
+        // Input
+        System.out.println("Enter the first number: ");
+        int a = sc.nextInt();
+
+        System.out.println();
+        System.out.println("Enter the second number: ");
+        int b = sc.nextInt();
+
+        // Solution
+        Solution solution = new Solution();
+        int largest = solution.largestOfTwoNumbers(a, b);
+
+        // Output - Main Problem
+        System.out.println();
+        System.out.println("The largest of \"a: " + a + "\" and \"b: " + b + "\" is: ");
+        System.out.println(largest);
+
+        sc.close();
+    }
+}
---
+++ 007-print-largest-of-two-numbers/001-solution/Solution.java
@@ -0,0 +1,24 @@
+/**
+ * Problem: Print the largest of two numbers.
+ */
+
+public class Solution {
+    public int largestOfTwoNumbers(int a, int b) {
+        if (a > b) {
+            return a;
+        }
+
+        return b;
+    }
+}
+
+/*
+ * Enter the first number:
+ * 12
+ * 
+ * Enter the second number:
+ * 14
+ * 
+ * The largest of "a: 12" and "b: 14" is:
+ * 14
+ */
