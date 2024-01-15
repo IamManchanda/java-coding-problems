cb8c61514574e051b4a2242a327c604d14772b7d
Harry Manchanda <harmanmanchanda182@gmail.com>
2024-01-15 14:55:09 +0530

    init

---
+++ 001-sum-two-integers/001-solution/Main.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Input
+        System.out.println("Enter the first number: ");
+        int a = sc.nextInt();
+
+        System.out.println();
+        System.out.println("Enter the second number: ");
+        int b = sc.nextInt();
+
+        // Solution
+        Solution solution = new Solution();
+        int sum = solution.getSum(a, b);
+
+        // Output
+        System.out.println();
+        System.out.println("The sum of " + a + " and " + b + " is: ");
+        System.out.println(sum);
+
+        sc.close();
+    }
+}
