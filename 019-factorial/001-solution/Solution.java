363fb9ebf8c8790c168b41095fbd8fa2c336cb0b
Harry Manchanda <harmanmanchanda182@gmail.com>
2024-01-16 16:38:26 +0530

    Find factorial of a number

---
+++ 019-factorial/001-solution/Solution.java
@@ -0,0 +1,23 @@
+/**
+ * Problem: Find the factorial of the number.
+ */
+
+public class Solution {
+    public int factorial(int number) {
+        int fact = 1;
+
+        for (int i = 1; i <= number; i++) {
+            fact *= i;
+        }
+
+        return fact;
+    }
+}
+
+/*
+ * Enter number:
+ * 5
+ * 
+ * Result:
+ * 120
+ */
