c24ee40d11e4fa831a60a87bdc97a939e9c1fdc6
Harry Manchanda <harmanmanchanda182@gmail.com>
2024-01-16 17:40:08 +0530

    Print Half Pyramid

---
+++ 023-print-half-pyramid/001-solution/Solution.java
@@ -0,0 +1,28 @@
+/**
+ * Problem: Print Half Pyramid.
+ */
+
+public class Solution {
+    public void printHalfPyramid(int lines) {
+        for (int row = 1; row <= lines; row++) {
+            for (int col = 1; col <= row; col++) {
+                System.out.print(col);
+            }
+            System.out.println();
+        }
+    }
+}
+
+/*
+ * Enter number of lines:
+ * 7
+ * 
+ * Result:
+ * 1
+ * 12
+ * 123
+ * 1234
+ * 12345
+ * 123456
+ * 1234567
+ */
