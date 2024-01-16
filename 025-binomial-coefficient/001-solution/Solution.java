25d43a82c06c0f4cbf7406ebfde8c6d36acaa2c2
Harry Manchanda <harmanmanchanda182@gmail.com>
2024-01-16 18:51:08 +0530

    working solution for binomial coefficient

---
+++ 025-binomial-coefficient/001-solution/Solution.java
@@ -0,0 +1,30 @@
+/**
+ * Problem: Find the binomial coefficient of n and k.
+ */
+
+public class Solution {
+    public int binomialCoefficient(int n, int k) {
+        return factorial(n) / (factorial(k) * factorial(n - k));
+    }
+
+    public int factorial(int number) {
+        int fact = 1;
+
+        for (int i = 1; i <= number; i++) {
+            fact *= i;
+        }
+
+        return fact;
+    }
+}
+
+/*
+ * Enter n:
+ * 7
+ * 
+ * Enter k:
+ * 3
+ * 
+ * Result:
+ * 35
+ */
