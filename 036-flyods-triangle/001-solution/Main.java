d1bcd491f9962714d26f5b60932f9d451d38bfab
Harry Manchanda <harmanmanchanda182@gmail.com>
2024-01-17 12:47:40 +0530

    Solve Problem: Print Flyod's Triangle

---
+++ 036-flyods-triangle/001-solution/Main.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Input
+        System.out.println("Enter the number of lines: ");
+        int lines = sc.nextInt();
+
+        // Solution & Output
+        Solution solution = new Solution();
+        System.out.println();
+        System.out.println("Result:");
+        solution.printFloydsTriangle(lines);
+
+        sc.close();
+    }
+}
---
+++ 036-flyods-triangle/001-solution/Solution.java
@@ -0,0 +1,31 @@
+/**
+ * Problem: Print a floyd's triangle pattern.
+ */
+
+public class Solution {
+    public void printFloydsTriangle(int lines) {
+        int num = 1;
+        for (int row = 1; row <= lines; row++) {
+            for (int col = 1; col <= row; col++) {
+                System.out.print(num + " ");
+                num++;
+            }
+
+            System.out.println();
+        }
+    }
+}
+
+/*
+ * Enter the number of lines:
+ * 7
+ * 
+ * Result:
+ * 1
+ * 2 3
+ * 4 5 6
+ * 7 8 9 10
+ * 11 12 13 14 15
+ * 16 17 18 19 20 21
+ * 22 23 24 25 26 27 28
+ */
