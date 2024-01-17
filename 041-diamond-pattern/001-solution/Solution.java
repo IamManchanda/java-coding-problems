65d9c628fa848743869c72f3709eb70c28dddf99
Harry Manchanda <harmanmanchanda182@gmail.com>
2024-01-17 15:10:50 +0530

    Remove un-necessary bracket

--- 041-diamond-pattern/001-solution/Solution.java
+++ 041-diamond-pattern/001-solution/Solution.java
@@ -18,7 +18,7 @@ public class Solution {
             System.out.print(" ");
         }
 
-        for (int j = 1; j <= ((2 * i) - 1); j++) {
+        for (int j = 1; j <= (2 * i) - 1; j++) {
             System.out.print("*");
         }
 
