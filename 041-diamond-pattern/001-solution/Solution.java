dcf3a2a8d945b533ee839d2c7b47233102b30c5a
Harry Manchanda <harmanmanchanda182@gmail.com>
2024-01-17 15:05:32 +0530

    Solve Diamond Pattern Problem

---
+++ 041-diamond-pattern/001-solution/Solution.java
@@ -0,0 +1,46 @@
+/**
+ * Problem: Print the Diamond Pattern
+ */
+
+public class Solution {
+    public void printDiamondPattern(int n) {
+        for (int i = 1; i <= n; i++) {
+            printDiamondPattern(i, n);
+        }
+
+        for (int i = n; i >= 1; i--) {
+            printDiamondPattern(i, n);
+        }
+    }
+
+    private void printDiamondPattern(int i, int n) {
+        for (int j = 1; j <= n - i; j++) {
+            System.out.print(" ");
+        }
+
+        for (int j = 1; j <= ((2 * i) - 1); j++) {
+            System.out.print("*");
+        }
+
+        System.out.println();
+    }
+}
+
+/*
+ * Enter n for the Diamond Pattern:
+ * 5
+ * 
+ * Result:
+ * ____*
+ * ___***
+ * __*****
+ * _*******
+ * *********
+ * *********
+ * _*******
+ * __*****
+ * ___***
+ * ____*
+ *
+ * _ denotes space
+ */
