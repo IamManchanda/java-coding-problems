cbd704cf9aff544dbaf45b83a6eb30583ce73ebf
Harry Manchanda <harmanmanchanda182@gmail.com>
2024-01-18 13:20:01 +0530

    Solve problem: Pairs in an array

---
+++ 046-pairs-array/001-solution/Solution.java
@@ -0,0 +1,56 @@
+/**
+ * Problem: Pairs in an array
+ */
+
+public class Solution {
+    public String[] pairsArray(int[] numbers) {
+        int resultsSize = binomialCoefficient(numbers.length, 2);
+        String[] results = new String[resultsSize];
+        int resultsIndex = 0;
+
+        for (int i = 0; i < numbers.length; i++) {
+            int current = numbers[i];
+
+            for (int j = i + 1; j < numbers.length; j++) {
+                int next = numbers[j];
+                results[resultsIndex] = "(" + current + ", " + next + ")";
+                resultsIndex++;
+            }
+        }
+
+        return results;
+    }
+
+    private int binomialCoefficient(int n, int k) {
+        return factorial(n) / (factorial(k) * factorial(n - k));
+    }
+
+    private int factorial(int number) {
+        int fact = 1;
+
+        for (int i = 1; i <= number; i++) {
+            fact *= i;
+        }
+
+        return fact;
+    }
+}
+
+/*
+ * Enter number of items in an array:
+ * 5
+ * 
+ * Enter items:
+ * Enter item 1: 2
+ * Enter item 2: 4
+ * Enter item 3: 6
+ * Enter item 4: 8
+ * Enter item 5: 10
+ * 
+ * Original Input (as an array):
+ * [2, 4, 6, 8, 10]
+ * 
+ * Result (as an array):
+ * [(2, 4), (2, 6), (2, 8), (2, 10), (4, 6), (4, 8), (4, 10), (6, 8), (6, 10),
+ * (8, 10)]
+ */
