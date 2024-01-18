4dd6bbaf0126c681ea455781595133432f10508c
Harry Manchanda <harmanmanchanda182@gmail.com>
2024-01-18 22:40:33 +0530

    Bubble sort

---
+++ 051-bubble-sort/001-solution/Solution.java
@@ -0,0 +1,46 @@
+/**
+ * Bubble Sort
+ * 
+ * Time Complexity: O(n^2)
+ * Space Complexity: O(1)
+ */
+
+public class Solution {
+    public int[] bubbleSort(int[] numbers) {
+        int n = numbers.length;
+
+        for (int i = 0; i <= n - 2; i++) {
+            for (int j = 0; j <= n - 2 - i; j++) {
+                if (numbers[j] > numbers[j + 1]) {
+                    swap(numbers, j, j + 1);
+                }
+            }
+        }
+
+        return numbers;
+    }
+
+    private void swap(int[] numbers, int x, int y) {
+        int temp = numbers[x];
+        numbers[x] = numbers[y];
+        numbers[y] = temp;
+    }
+}
+
+/*
+ * Enter number of items:
+ * 5
+ * 
+ * Enter items:
+ * Enter item 1: 5
+ * Enter item 2: 4
+ * Enter item 3: 1
+ * Enter item 4: 3
+ * Enter item 5: 2
+ * 
+ * Original Input (as an array):
+ * [5, 4, 1, 3, 2]
+ * 
+ * Result (as an array):
+ * [1, 2, 3, 4, 5]
+ */
