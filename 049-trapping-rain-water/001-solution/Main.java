622f64e2624715842c060994f2a1bdfb067b9f25
Harry Manchanda <harmanmanchanda182@gmail.com>
2024-01-18 20:12:10 +0530

    Solve Problem: Trapping rain water

---
+++ 049-trapping-rain-water/001-solution/Main.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Input
+        System.out.println("Enter number of items in the `height` array: ");
+        int n = sc.nextInt();
+
+        System.out.println();
+        System.out.println("Enter items: ");
+        int[] height = new int[n];
+        for (int i = 0; i < n; i++) {
+            System.out.print("Enter item " + (i + 1) + ": ");
+            height[i] = sc.nextInt();
+        }
+
+        // Input Log before solution is applied
+        System.out.println();
+        System.out.println("Original Input (`height` array):");
+        System.out.println(Arrays.toString(height));
+
+        // Solution
+        Solution solution = new Solution();
+        int result = solution.trap(height);
+
+        System.out.println();
+        System.out.println("Result:");
+        System.out.println(result);
+
+        sc.close();
+    }
+}
---
+++ 049-trapping-rain-water/001-solution/Solution.java
@@ -0,0 +1,89 @@
+/**
+ * Problem: Trapping rain water
+ * https://leetcode.com/problems/trapping-rain-water/submissions/1149824417/
+ * 
+ * Time complexity: O(n)
+ * Space complexity: O(n)
+ */
+
+public class Solution {
+    public int trap(int[] height) {
+        int trappedWater = 0;
+        int n = height.length;
+        int[] leftMax = getLeftMaxBoundaryList(height, n);
+        int[] rightMax = getRightMaxBoundaryList(height, n);
+
+        for (int i = 0; i < n; i++) {
+            int waterLevel = Math.min(leftMax[i], rightMax[i]);
+            trappedWater += waterLevel - height[i];
+        }
+
+        return trappedWater;
+    }
+
+    private int[] getLeftMaxBoundaryList(int[] height, int n) {
+        int[] leftMax = new int[n];
+        leftMax[0] = height[0];
+
+        for (int i = 1; i < n; i++) {
+            leftMax[i] = Math.max(height[i], leftMax[i - 1]);
+        }
+
+        return leftMax;
+    }
+
+    private int[] getRightMaxBoundaryList(int[] height, int n) {
+        int[] rightMax = new int[n];
+        rightMax[n - 1] = height[n - 1];
+
+        for (int i = n - 2; i >= 0; i--) {
+            rightMax[i] = Math.max(height[i], rightMax[i + 1]);
+        }
+
+        return rightMax;
+    }
+}
+
+/*
+ * Enter number of items in the `height` array:
+ * 12
+ * 
+ * Enter items:
+ * Enter item 1: 0
+ * Enter item 2: 1
+ * Enter item 3: 0
+ * Enter item 4: 2
+ * Enter item 5: 1
+ * Enter item 6: 0
+ * Enter item 7: 1
+ * Enter item 8: 3
+ * Enter item 9: 2
+ * Enter item 10: 1
+ * Enter item 11: 2
+ * Enter item 12: 1
+ * 
+ * Original Input (`height` array):
+ * [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]
+ * 
+ * Result:
+ * 6
+ */
+
+/*
+ * Enter number of items in the `height` array:
+ * 6
+ * 
+ * Enter items:
+ * Enter item 1: 4
+ * Enter item 2: 2
+ * Enter item 3: 0
+ * Enter item 4: 3
+ * Enter item 5: 2
+ * Enter item 6: 5
+ * 
+ * Original Input (`height` array):
+ * [4, 2, 0, 3, 2, 5]
+ * 
+ * Result:
+ * 9
+ */
