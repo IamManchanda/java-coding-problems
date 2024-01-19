03a7e34723658b82866f6b7e4f85d3e164ad6025
Harry Manchanda <harmanmanchanda182@gmail.com>
2024-01-19 16:43:43 +0530

    Solve problem: Contains duplicate

---
+++ 056-contains-duplicate/002-solution/Main.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Input
+        System.out.println("Enter number of items: ");
+        int n = sc.nextInt();
+
+        System.out.println();
+        System.out.println("Enter items: ");
+        int[] nums = new int[n];
+        for (int i = 0; i < n; i++) {
+            System.out.print("Enter item " + (i + 1) + ": ");
+            nums[i] = sc.nextInt();
+        }
+
+        // Input Log before solution is applied
+        System.out.println();
+        System.out.println("Original Input (as an array):");
+        System.out.println(Arrays.toString(nums));
+
+        // Solution
+        Solution solution = new Solution();
+        boolean result = solution.containsDuplicate(nums);
+
+        System.out.println();
+        System.out.println("Result:");
+        System.out.println(result);
+
+        sc.close();
+    }
+}
---
+++ 056-contains-duplicate/002-solution/Solution.java
@@ -0,0 +1,83 @@
+import java.util.*;
+
+/**
+ * Contains Duplicate
+ * https://leetcode.com/problems/contains-duplicate/submissions/1150629492/
+ * 
+ * Approach: Using Sets
+ * - Time complexity: O(n)
+ * - Space complexity: O(n)
+ */
+
+public class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+
+        for (int num : nums) {
+            if (set.contains(num)) {
+                return true;
+            }
+
+            set.add(num);
+        }
+
+        return false;
+    }
+}
+
+/*
+ * Enter number of items:
+ * 4
+ * 
+ * Enter items:
+ * Enter item 1: 1
+ * Enter item 2: 2
+ * Enter item 3: 3
+ * Enter item 4: 1
+ * 
+ * Original Input (as an array):
+ * [1, 2, 3, 1]
+ * 
+ * Result:
+ * true
+ */
+
+/*
+ * Enter number of items:
+ * 4
+ * 
+ * Enter items:
+ * Enter item 1: 1
+ * Enter item 2: 2
+ * Enter item 3: 3
+ * Enter item 4: 4
+ * 
+ * Original Input (as an array):
+ * [1, 2, 3, 4]
+ * 
+ * Result:
+ * false
+ */
+
+/*
+ * Enter number of items:
+ * 10
+ * 
+ * Enter items:
+ * Enter item 1: 1
+ * Enter item 2: 1
+ * Enter item 3: 1
+ * Enter item 4: 3
+ * Enter item 5: 3
+ * Enter item 6: 4
+ * Enter item 7: 3
+ * Enter item 8: 2
+ * Enter item 9: 4
+ * Enter item 10: 2
+ * 
+ * Original Input (as an array):
+ * [1, 1, 1, 3, 3, 4, 3, 2, 4, 2]
+ * 
+ * Result:
+ * true
+ */
