5d8b4515994b44f7c21428068f60742826fc93e5
Harry Manchanda <harmanmanchanda182@gmail.com>
2024-01-23 13:57:11 +0530

    Valid Palindrome updates

--- 066-valid-palindrome/001-solution/Solution.java
+++ 066-valid-palindrome/001-solution/Solution.java
@@ -10,12 +10,13 @@ public class Solution {
     public boolean isPalindrome(String s) {
         String str = removeNonAlphanumeric(s);
         int n = str.length();
+        int mid = findMidIndex(0, n - 1);
 
         if (str.isEmpty()) {
             return true;
         }
 
-        for (int i = 0; i <= n / 2; i++) {
+        for (int i = 0; i <= mid; i++) {
             if (str.charAt(i) != str.charAt(n - i - 1)) {
                 return false;
             }
@@ -28,6 +29,11 @@ public class Solution {
         return input
                 .replaceAll("[^a-zA-Z0-9]", "")
                 .toLowerCase();
+    }
+
+    private int findMidIndex(int start, int end) {
+        // Avoids overflow compared to (start + end) / 2
+        return start + (end - start) / 2;
     }
 }
 
