27e8138410e41595393f0bc8d502ce4b1154a825
Harry Manchanda <harmanmanchanda182@gmail.com>
2024-01-24 23:14:45 +0530

    bit manipulation playground updates

---
+++ 074-bit-manipulation/001-playground/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int binAnd = 5 & 6;
+        System.out.println("5 & 6 = " + binAnd); // 4
+
+        int binOr = 5 | 6;
+        System.out.println("5 | 6 = " + binOr); // 7
+
+        int binXor = 5 ^ 6;
+        System.out.println("5 ^ 6 = " + binXor); // 3
+    }
+}
