4efa8b023ec2dd6e5dd9b25207183dc754df0989
Harry Manchanda <harmanmanchanda182@gmail.com>
2024-02-09 12:13:22 +0530

    N-Queens, count ways

---
+++ 122-n-queens-count-ways/001-solution/Main.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Input
+        System.out.println("Enter n:");
+        int n = sc.nextInt();
+
+        // Solution
+        Solution solution = new Solution();
+        int result = solution.nQueens(n);
+
+        System.out.println();
+        System.out.println("Result:");
+        System.out.println(result);
+
+        sc.close();
+    }
+}
---
+++ 122-n-queens-count-ways/001-solution/Solution.java
@@ -0,0 +1,79 @@
+/**
+ * N-Queens: Count all possible solutions for
+ * placing n queens on an n x n chessboard.
+ * 
+ * Approach: Backtracking
+ * - Time Complexity: O(n!)
+ * - Space Complexity: O(n^2)
+ * Note: For learning purposes, Bad time/space complexity is expected.
+ */
+
+public class Solution {
+    private static int count = 0;
+
+    public int nQueens(int n) {
+        char[][] board = new char[n][n];
+
+        for (int i = 0; i <= n - 1; i++) {
+            for (int j = 0; j <= n - 1; j++) {
+                board[i][j] = 'x';
+            }
+        }
+
+        nQueens(board, 0);
+
+        return count;
+    }
+
+    private void nQueens(char[][] board, int i) {
+        int n = board.length;
+
+        if (i == n) {
+            count++;
+            return;
+        }
+
+        for (int j = 0; j <= n - 1; j++) {
+            if (isSafe(board, i, j)) {
+                board[i][j] = 'Q';
+                nQueens(board, i + 1);
+                board[i][j] = 'x';
+            }
+        }
+    }
+
+    private boolean isSafe(char[][] board, int i, int j) {
+        int n = board.length;
+
+        // Check vertically up
+        for (int k = n - 1; k >= 0; k--) {
+            if (board[k][j] == 'Q') {
+                return false;
+            }
+        }
+
+        // Check diagonally left up
+        for (int k = i - 1, l = j - 1; k >= 0 && l >= 0; k--, l--) {
+            if (board[k][l] == 'Q') {
+                return false;
+            }
+        }
+
+        // Check diagonally right up
+        for (int k = i - 1, l = j + 1; k >= 0 && l <= n - 1; k--, l++) {
+            if (board[k][l] == 'Q') {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
+
+/*
+ * Enter n:
+ * 4
+ * 
+ * Result:
+ * 2
+ */
