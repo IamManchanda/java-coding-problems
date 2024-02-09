18a57b4fcd4710eaffa1b710552d710bc8310483
Harry Manchanda <harmanmanchanda182@gmail.com>
2024-02-09 13:11:40 +0530

    N-Queens: Print one possible solution

---
+++ 123-n-queens-print-one-way/001-solution/Main.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Input
+        System.out.println("Enter n:");
+        int n = sc.nextInt();
+
+        // Solution & Output
+        Solution solution = new Solution();
+        System.out.println();
+        System.out.println("Result:");
+        solution.nQueens(n);
+
+        sc.close();
+    }
+}
---
+++ 123-n-queens-print-one-way/001-solution/Solution.java
@@ -0,0 +1,103 @@
+/**
+ * N-Queens: Print one possible solution for
+ * placing n queens on an n x n chessboard.
+ */
+
+public class Solution {
+    public void nQueens(int n) {
+        char[][] board = new char[n][n];
+
+        for (int i = 0; i <= n - 1; i++) {
+            for (int j = 0; j <= n - 1; j++) {
+                board[i][j] = 'x';
+            }
+        }
+
+        if (checkNQueens(board, 0)) {
+            System.out.println("Solution exists.");
+            printBoard(board);
+        } else {
+            System.out.println("No solution exists.");
+        }
+    }
+
+    private boolean checkNQueens(char[][] board, int i) {
+        int n = board.length;
+
+        if (i == n) {
+            return true;
+        }
+
+        for (int j = 0; j <= n - 1; j++) {
+            if (isSafe(board, i, j)) {
+                board[i][j] = 'Q';
+                if (checkNQueens(board, i + 1)) {
+                    return true;
+                }
+                board[i][j] = 'x';
+            }
+        }
+
+        return false;
+    }
+
+    private boolean isSafe(char[][] board, int i, int j) {
+        int n = board.length;
+
+        // Check vertically up
+        for (int k = n - 1; k >= 0; k--) {
+            if (board[k][j] == 'Q') {
+                return false;
+            }
+        }
+
+        // Check diagonally left up
+        for (int k = i - 1, l = j - 1; k >= 0 && l >= 0; k--, l--) {
+            if (board[k][l] == 'Q') {
+                return false;
+            }
+        }
+
+        // Check diagonally right up
+        for (int k = i - 1, l = j + 1; k >= 0 && l <= n - 1; k--, l++) {
+            if (board[k][l] == 'Q') {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private void printBoard(char[][] board) {
+        int n = board.length;
+
+        System.out.println("-----Chess Board-----");
+        for (int i = 0; i <= n - 1; i++) {
+            for (int j = 0; j <= n - 1; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+/*
+ * Enter n:
+ * 4
+ * 
+ * Result:
+ * Solution exists.
+ * -----Chess Board-----
+ * x Q x x
+ * x x x Q
+ * Q x x x
+ * x x Q x
+ */
+
+/*
+ * Enter n:
+ * 3
+ * 
+ * Result:
+ * No solution exists.
+ */
