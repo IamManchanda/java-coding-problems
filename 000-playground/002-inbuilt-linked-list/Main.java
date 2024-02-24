00c22521f5ec0a9f964fedfdca1066dbdabce325
Harry Manchanda <harmanmanchanda182@gmail.com>
2024-02-24 17:28:00 +0530

    Inbuilt Linked List

---
+++ 000-playground/002-inbuilt-linked-list/Main.java
@@ -0,0 +1,14 @@
+import java.util.LinkedList;
+
+public class Main {
+    public static void main(String[] args) {
+        LinkedList<Integer> ll = new LinkedList<>();
+        ll.add(1);
+        ll.add(2);
+        ll.addFirst(0);
+        System.out.println(ll);
+        ll.removeLast();
+        ll.removeFirst();
+        System.out.println(ll);
+    }
+}
