25aeaa6554e80cfa5b5cc3d8a18d928d163d3b19
Harry Manchanda <harmanmanchanda182@gmail.com>
2024-02-25 15:37:25 +0530

    doubly linked list

---
+++ 000-playground/002-custom-doubly-linked-list/CustomLinkedList.java
@@ -0,0 +1,64 @@
+public class CustomLinkedList {
+    public static class Node {
+        int data;
+        Node next;
+        Node prev;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+            this.prev = null;
+        }
+    }
+
+    public static Node head;
+    public static Node tail;
+    public static int count;
+
+    public void addFirst(int data) {
+        Node newNode = new Node(data);
+        count++;
+
+        if (head == null) {
+            head = tail = newNode;
+            return;
+        }
+
+        newNode.next = head;
+        head.prev = newNode;
+        head = newNode;
+    }
+
+    public void addLast(int data) {
+        Node newNode = new Node(data);
+        count++;
+
+        if (head == null) {
+            head = tail = newNode;
+            return;
+        }
+
+        tail.next = newNode;
+        newNode.prev = tail;
+        tail = newNode;
+    }
+
+    public void add(int data) {
+        addLast(data);
+    }
+
+    public void print() {
+        Node temp = head;
+
+        while (temp != null) {
+            System.out.print(temp.data + " <-> ");
+            temp = temp.next;
+        }
+
+        System.out.println("null");
+    }
+
+    public int size() {
+        return count;
+    }
+}
---
+++ 000-playground/002-custom-doubly-linked-list/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        CustomLinkedList dll = new CustomLinkedList();
+        dll.add(1);
+        dll.add(2);
+        dll.add(3);
+
+        dll.print();
+        System.out.println(dll.size());
+    }
+}
