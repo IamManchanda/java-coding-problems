56743d28bf480f3e8cc3deefab08ac2cf693d03d
Harry Manchanda <harmanmanchanda182@gmail.com>
2024-02-29 17:32:59 +0530

    Push an item at the bottom of the stack

---
+++ 174-push-bottom-stack/001-solution/Main.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Input
+        System.out.println("Enter number of items in Stack `s`: ");
+        int n = sc.nextInt();
+
+        System.out.println();
+        System.out.println("Enter items: ");
+        Stack<Integer> s = new Stack<>();
+
+        for (int i = 0; i < n; i++) {
+            System.out.print("Enter item " + (i + 1) + ": ");
+            s.push(sc.nextInt());
+        }
+
+        System.out.println();
+        System.out.println("Entered Stack `s`: ");
+        System.out.println(s);
+
+        System.out.println();
+        System.out.println("Enter `item` to push at bottom: ");
+        int item = sc.nextInt();
+
+        // Solution and Output
+        Solution solution = new Solution();
+        solution.pushAtBottom(s, item);
+        System.out.println();
+        System.out.println("Result:");
+        System.out.println(s);
+
+        sc.close();
+    }
+}
---
+++ 174-push-bottom-stack/001-solution/Solution.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+/**
+ * Push an item at the bottom of the stack
+ */
+
+public class Solution {
+    public void pushAtBottom(Stack<Integer> s, int item) {
+        if (s.isEmpty()) {
+            s.push(item);
+            return;
+        }
+
+        int top = s.pop();
+        pushAtBottom(s, item);
+        s.push(top);
+    }
+}
+
+/*
+ * Entered Stack `s`:
+ * [1, 2, 3]
+ * 
+ * Enter `item` to push at bottom:
+ * 4
+ * 
+ * Result:
+ * [4, 1, 2, 3]
+ */
