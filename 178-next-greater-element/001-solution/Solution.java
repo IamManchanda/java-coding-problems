b38f0f31f01045cede2830d1b7d1617b2d8c8dff
Harry Manchanda <harmanmanchanda182@gmail.com>
2024-03-01 14:46:32 +0530

    Solve Problem: Next Greater Element

---
+++ 178-next-greater-element/001-solution/Solution.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+/**
+ * Next Greater Element
+ */
+
+public class Solution {
+    public int[] nextGreaterElements(int[] nums) {
+        Stack<Integer> st = new Stack<>();
+        int n = nums.length;
+        int[] nextGreater = new int[n];
+
+        for (int i = n - 1; i >= 0; i--) {
+            while (!st.isEmpty() && nums[st.peek()] <= nums[i]) {
+                st.pop();
+            }
+
+            nextGreater[i] = st.isEmpty() ? -1 : nums[st.peek()];
+
+            st.push(i);
+        }
+
+        return nextGreater;
+    }
+}
+
+/*
+ * Entered items in `nums`:
+ * [6, 8, 0, 1, 3]
+ * 
+ * Result:
+ * [8, -1, 1, 3, -1]
+ */
