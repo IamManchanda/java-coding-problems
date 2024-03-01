8109d05e346b668ec7c1af0e01b755fc3b5cf753
Harry Manchanda <harmanmanchanda182@gmail.com>
2024-03-01 19:52:15 +0530

    Solve Duplicate Parentheses problem

---
+++ 180-duplicate-parentheses/001-solution/Solution.java
@@ -0,0 +1,61 @@
+import java.util.*;
+
+/**
+ * Duplicate Parentheses
+ * https://www.codingninjas.com/studio/problems/duplicate-parenthesis_2663296
+ * 
+ * Approach: Using Stack
+ * - Time complexity: O(n)
+ * - Space complexity: O(n)
+ */
+
+public class Solution {
+    public boolean duplicateParanthesis(String expr) {
+        Stack<Character> st = new Stack<>();
+
+        for (char ch : expr.toCharArray()) {
+            if (ch == ')') {
+                int count = 0;
+
+                while (st.peek() != '(') {
+                    st.pop();
+                    count++;
+                }
+
+                if (count < 1) {
+                    return true;
+                } else {
+                    st.pop();
+                }
+            } else {
+                st.push(ch);
+            }
+        }
+
+        return false;
+    }
+}
+
+/*
+ * Enter String `s`:
+ * (a+b)+((c+d))
+ * 
+ * Result:
+ * true
+ */
+
+/*
+ * Enter String `s`:
+ * ((a+b) + c)
+ * 
+ * Result:
+ * false
+ */
+
+/*
+ * Enter String `s`:
+ * ((a+b)+(c+d))
+ * 
+ * Result:
+ * false
+ */
