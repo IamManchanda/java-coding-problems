3e846c9ec5e41eeef4bc5ed554115f57b36b959a
Harry Manchanda <harmanmanchanda182@gmail.com>
2024-03-02 20:41:03 +0530

    Inbuilt stack updates

--- 000-playground/006-inbuilt-stack/Main.java
+++ 000-playground/006-inbuilt-stack/Main.java
@@ -2,7 +2,9 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        Stack<Integer> s = new Stack<>();
+        ArrayDeque<Integer> s = new ArrayDeque<>();
+        // or, Stack<Integer> s = new Stack<>();
+
         s.push(1);
         s.push(2);
         s.push(3);
