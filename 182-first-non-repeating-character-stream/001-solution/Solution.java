ad4d88b632ef8737e0777a03add2f368a624a133
Harry Manchanda <harmanmanchanda182@gmail.com>
2024-03-02 23:29:35 +0530

    Solve Problem: First non-repeating character in a stream

---
+++ 182-first-non-repeating-character-stream/001-solution/Solution.java
@@ -0,0 +1,59 @@
+import java.util.*;
+
+/**
+ * First non-repeating character in a stream
+ * https://www.geeksforgeeks.org/problems/first-non-repeating-character-in-a-stream1216/1
+ * 
+ * Approach: Hash Table (int[26]) and Queue (LinkedList)
+ * - Time complexity: O(n)
+ * - Space complexity: O(n)
+ */
+
+public class Solution {
+    public String FirstNonRepeating(String A) {
+        StringBuilder result = new StringBuilder("");
+        int[] freq = new int[26]; // a-z
+        Queue<Character> q = new LinkedList<>();
+
+        for (char ch : A.toCharArray()) {
+            q.add(ch);
+            freq[ch - 'a']++;
+
+            while (!q.isEmpty() && freq[q.peek() - 'a'] > 1) {
+                q.remove();
+            }
+
+            if (q.isEmpty()) {
+                result.append("#");
+            } else {
+                result.append(q.peek());
+            }
+        }
+
+        return result.toString();
+    }
+}
+
+/*
+ * Enter String `s`:
+ * aabc
+ * 
+ * Result:
+ * a#bb
+ */
+
+/*
+ * Enter String `s`:
+ * zz
+ * 
+ * Result:
+ * z#
+ */
+
+/*
+ * Enter String `s`:
+ * aabccxb
+ * 
+ * Result:
+ * a#bbbbx
+ */
