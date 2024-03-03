63ed2d9757f531a913c0fb5289789a4dd5520fd3
Harry Manchanda <harmanmanchanda182@gmail.com>
2024-03-03 16:36:45 +0530

    Code updates: Inbuilt Stack

--- 000-playground/006-inbuilt-stack/Main.java
+++ 000-playground/006-inbuilt-stack/Main.java
@@ -2,16 +2,16 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayDeque<Integer> s = new ArrayDeque<>();
-        // or, Stack<Integer> s = new Stack<>();
+        Deque<Integer> st = new ArrayDeque<>();
+        // or, Stack<Integer> st = new Stack<>();
 
-        s.push(1);
-        s.push(2);
-        s.push(3);
+        st.push(1);
+        st.push(2);
+        st.push(3);
 
-        while (!s.isEmpty()) {
-            System.out.println(s.peek());
-            s.pop();
+        while (!st.isEmpty()) {
+            System.out.println(st.peek());
+            st.pop();
         }
     }
 }
