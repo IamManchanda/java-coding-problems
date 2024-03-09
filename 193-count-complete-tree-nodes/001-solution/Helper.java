dfe684e43fa62a77ae9d26b090a11dedb93bc48e
Harry Manchanda <harmanmanchanda182@gmail.com>
2024-03-09 23:18:52 +0530

    Create Tree updates

--- 193-count-complete-tree-nodes/001-solution/Helper.java
+++ 193-count-complete-tree-nodes/001-solution/Helper.java
@@ -1,22 +1,27 @@
 public class Helper {
     public TreeNode createTree(int[] root) {
-        if (root.length == 0) {
+        int n = root.length;
+
+        if (n == 0) {
             return null;
         }
 
-        TreeNode[] nodes = new TreeNode[root.length];
+        TreeNode[] nodes = new TreeNode[n];
 
-        for (int i = 0; i < root.length; i++) {
+        for (int i = 0; i <= n - 1; i++) {
             nodes[i] = new TreeNode(root[i]);
         }
 
-        for (int i = 0; i < root.length; i++) {
-            if (2 * i + 1 < root.length) {
-                nodes[i].left = nodes[2 * i + 1];
+        for (int i = 0; i <= n - 1; i++) {
+            int leftIndex = 2 * i + 1;
+            int rightIndex = 2 * i + 2;
+
+            if (leftIndex < n) {
+                nodes[i].left = nodes[leftIndex];
             }
 
-            if (2 * i + 2 < root.length) {
-                nodes[i].right = nodes[2 * i + 2];
+            if (rightIndex < n) {
+                nodes[i].right = nodes[rightIndex];
             }
         }
 
