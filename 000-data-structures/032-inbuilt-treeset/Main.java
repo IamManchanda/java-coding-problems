c37b28bb5ecfe065bc8d43523604423656797bc2
Harry Manchanda <harmanmanchanda182@gmail.com>
2024-03-24 18:51:52 +0530

    Inbuilt LinkedHashSet and TreeSet

---
+++ 000-data-structures/032-inbuilt-treeset/Main.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Set<String> hs = new HashSet<>();
+        hs.add("Delhi");
+        hs.add("Mumbai");
+        hs.add("Noida");
+        hs.add("Bengaluru");
+        System.out.println(hs);
+
+        Set<String> lhs = new LinkedHashSet<>();
+        lhs.add("Delhi");
+        lhs.add("Mumbai");
+        lhs.add("Noida");
+        lhs.add("Bengaluru");
+        System.out.println(lhs);
+
+        Set<String> ts = new TreeSet<>();
+        ts.add("Delhi");
+        ts.add("Mumbai");
+        ts.add("Noida");
+        ts.add("Bengaluru");
+        System.out.println(ts);
+    }
+}
