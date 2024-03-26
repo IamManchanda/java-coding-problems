1caa275865ad567aaf09e63280a7d5c74dc7b8da
Harry Manchanda <harmanmanchanda182@gmail.com>
2024-03-27 00:08:26 +0530

    Code comment updates

--- 050-buy-sell-stocks/001-solution/Solution.java
+++ 050-buy-sell-stocks/001-solution/Solution.java
@@ -25,17 +25,6 @@ public class Solution {
 }
 
 /*
- * Enter number of items in the `prices` array:
- * 6
- * 
- * Enter items:
- * Enter item 1: 7
- * Enter item 2: 1
- * Enter item 3: 5
- * Enter item 4: 3
- * Enter item 5: 6
- * Enter item 6: 4
- * 
  * Original Input (`prices` array):
  * [7, 1, 5, 3, 6, 4]
  * 
@@ -44,16 +33,6 @@ public class Solution {
  */
 
 /*
- * Enter number of items in the `prices` array:
- * 5
- * 
- * Enter items:
- * Enter item 1: 7
- * Enter item 2: 6
- * Enter item 3: 4
- * Enter item 4: 3
- * Enter item 5: 1
- * 
  * Original Input (`prices` array):
  * [7, 6, 4, 3, 1]
  * 
