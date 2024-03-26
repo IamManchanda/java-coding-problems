01c986d7d8cf12e46decdd5ec070edffe6ca4a4a
Harry Manchanda <harmanmanchanda182@gmail.com>
2024-03-27 00:20:24 +0530

    Isolate Desc/Asc bubble sort functions

--- 051-bubble-sort/001-solution/Solution.java
+++ 051-bubble-sort/001-solution/Solution.java
@@ -7,6 +7,12 @@
 
 public class Solution {
     public int[] bubbleSort(int[] numbers, boolean isDescending) {
+        return isDescending
+                ? bubbleSortDescending(numbers)
+                : bubbleSortAscending(numbers);
+    }
+
+    private int[] bubbleSortAscending(int[] numbers) {
         int n = numbers.length;
         boolean swapped;
 
@@ -14,9 +20,29 @@ public class Solution {
             swapped = false;
 
             for (int j = 0; j <= n - 2 - i; j++) {
-                if (isDescending
-                        ? numbers[j] < numbers[j + 1]
-                        : numbers[j] > numbers[j + 1]) {
+                if (numbers[j] > numbers[j + 1]) {
+                    swap(numbers, j, j + 1);
+                    swapped = true;
+                }
+            }
+
+            if (!swapped) {
+                break;
+            }
+        }
+
+        return numbers;
+    }
+
+    private int[] bubbleSortDescending(int[] numbers) {
+        int n = numbers.length;
+        boolean swapped;
+
+        for (int i = 0; i <= n - 2; i++) {
+            swapped = false;
+
+            for (int j = 0; j <= n - 2 - i; j++) {
+                if (numbers[j] < numbers[j + 1]) {
                     swap(numbers, j, j + 1);
                     swapped = true;
                 }
@@ -37,20 +63,7 @@ public class Solution {
     }
 }
 
-/*
- * Enter number of items:
- * 5
- * 
- * Enter items:
- * Enter item 1: 5
- * Enter item 2: 4
- * Enter item 3: 1
- * Enter item 4: 3
- * Enter item 5: 2
- * 
- * Enter the sorting order (1 for ASC, 2 for DESC):
- * 1
- * 
+/**
  * Sorting order: ASC
  * 
  * Original Input (as an array):
@@ -61,19 +74,6 @@ public class Solution {
  */
 
 /*
- * Enter number of items:
- * 5
- * 
- * Enter items:
- * Enter item 1: 5
- * Enter item 2: 4
- * Enter item 3: 1
- * Enter item 4: 3
- * Enter item 5: 2
- * 
- * Enter the sorting order (1 for ASC, 2 for DESC):
- * 2
- * 
  * Sorting order: DESC
  * 
  * Original Input (as an array):
