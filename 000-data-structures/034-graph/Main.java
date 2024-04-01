b770c500f6a27d7a7020938160ea3dde90a38d2a
Harry Manchanda <harmanmanchanda182@gmail.com>
2024-04-02 00:11:17 +0530

    Graph logic update

--- 000-data-structures/034-graph/Main.java
+++ 000-data-structures/034-graph/Main.java
@@ -2,38 +2,12 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        int V = 5;
-
-        @SuppressWarnings("unchecked")
-        ArrayList<Edge>[] graph = new ArrayList[V];
-
-        for (int i = 0; i < V; i++) {
-            graph[i] = new ArrayList<>();
-        }
-
-        // Vertex 0
-        graph[0].add(new Edge(0, 1, 5));
-
-        // Vertex 1
-        graph[1].add(new Edge(1, 0, 5));
-        graph[1].add(new Edge(1, 2, 1));
-        graph[1].add(new Edge(1, 3, 3));
-
-        // Vertex 2
-        graph[2].add(new Edge(2, 1, 1));
-        graph[2].add(new Edge(2, 3, 1));
-        graph[2].add(new Edge(2, 4, 4));
-
-        // Vertex 3
-        graph[3].add(new Edge(3, 1, 3));
-        graph[3].add(new Edge(3, 2, 1));
-
-        // Vertex 4
-        graph[4].add(new Edge(4, 2, 4));
+        Graph g = new Graph();
+        ArrayList<Edge>[] graph = g.create();
 
         // 2's neighbors
         for (Edge e : graph[2]) {
-            System.out.println(e);
+            System.out.println(e.src + " -> " + e.dest + " (" + e.weight + ")");
         }
     }
 }
