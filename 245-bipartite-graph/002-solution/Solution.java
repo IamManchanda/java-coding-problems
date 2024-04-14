bdf14590d8b87402be589b028a09d6320f5f9bf1
Harry Manchanda <harmanmanchanda182@gmail.com>
2024-04-14 16:08:15 +0530

    Simplify second solution

--- 245-bipartite-graph/002-solution/Solution.java
+++ 245-bipartite-graph/002-solution/Solution.java
@@ -3,7 +3,7 @@ import java.util.*;
 /**
  * Check if a graph is Bipartite.
  * 
- * Approach: Cycle Detection, then Even-Odd cycle detection.
+ * Approach: Depth First Search (DFS) on a graph.
  * - Time complexity: O(V + E)
  * - Space complexity: O(V)
  * where, V is the number of vertices
@@ -12,76 +12,33 @@ import java.util.*;
 
 public class Solution {
     public boolean isBipartite(List<Edge>[] graph) {
-        if (hasCycle(graph)) {
-            return false;
-        }
+        int V = graph.length;
+        int[] col = new int[V];
+        Arrays.fill(col, -1);
 
-        if (isOddCycle(graph)) {
-            return false;
-        }
-
-        return true; // Even cycle, so it is Bipartite
-    }
-
-    private boolean hasCycle(List<Edge>[] graph) {
-        int n = graph.length;
-        boolean[] visited = new boolean[n];
-
-        for (int i = 0; i < n; i++) {
-            if (!visited[i] && hasCycle(graph, visited, i, -1)) {
-                return true;
+        for (int i = 0; i < V; i++) {
+            if (col[i] == -1 && !isBipartite(graph, col, i, 0)) {
+                return false;
             }
         }
 
-        return false;
+        return true;
     }
 
-    private boolean hasCycle(List<Edge>[] graph, boolean[] visited, int current, int parent) {
-        visited[current] = true;
+    private boolean isBipartite(List<Edge>[] graph, int[] col, int curr, int color) {
+        if (col[curr] != -1) {
+            return col[curr] == color;
+        }
 
-        for (Edge edge : graph[current]) {
-            if (!visited[edge.dest]) {
-                if (hasCycle(graph, visited, edge.dest, current)) {
-                    return true;
-                }
-            } else if (visited[edge.dest] && edge.dest != parent) {
-                return true;
+        col[curr] = color;
+
+        for (Edge edge : graph[curr]) {
+            if (!isBipartite(graph, col, edge.dest, 1 - color)) {
+                return false;
             }
         }
 
-        return false;
-    }
-
-    private boolean isOddCycle(List<Edge>[] graph) {
-        int n = graph.length;
-        boolean[] visited = new boolean[n];
-        int[] color = new int[n];
-
-        for (int i = 0; i < n; i++) {
-            if (!visited[i] && isOddCycle(graph, visited, color, i, -1)) {
-                return true;
-            }
-        }
-
-        return false;
-    }
-
-    private boolean isOddCycle(List<Edge>[] graph, boolean[] visited, int[] color, int current, int parent) {
-        visited[current] = true;
-
-        for (Edge edge : graph[current]) {
-            if (!visited[edge.dest]) {
-                color[edge.dest] = color[current] == 1 ? 2 : 1;
-
-                if (isOddCycle(graph, visited, color, edge.dest, current)) {
-                    return true;
-                }
-            } else if (visited[edge.dest] && edge.dest != parent && color[edge.dest] == color[current]) {
-                return true;
-            }
-        }
-
-        return false;
+        return true;
     }
 }
 
